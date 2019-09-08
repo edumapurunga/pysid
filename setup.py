eb86996f78274a1c69d5f5e5a13deedefd7f83b3
Eduardo <edumapurunga@gmail.com>
2019-09-08 20:35:11 -0300

    Update: Add installation requirements in setup.py

--- setup.py
+++ setup.py
@@ -23,5 +23,7 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
+    install_requires=['numpy',
+                      'scipy'],
     python_requires='>=3.6',
 )
