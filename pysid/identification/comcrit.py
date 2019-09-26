ce0b0793b3e284c6aec661928ea4365607b64e28
Eduardo <edumapurunga@gmail.com>
2019-09-26 00:32:12 -0300

    Add: comcrit module - determination

---
+++ pysid/identification/comcrit.py
@@ -0,0 +1,9 @@
+# encoded utf-8
+"""
+This module provides tools for model determination. That includes the
+traditional information criteria, such as Akaike information criterion
+(AIC), BIC, Final predictor error (FPE) and other tools to determine
+model orders.
+
+author: @edumapurunga
+"""
