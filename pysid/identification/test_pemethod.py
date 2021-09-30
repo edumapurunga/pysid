d494275cbd70ae98bee207de3d748ccf2439f465
Pedro Rodrigues de Lima <pee.rodrigues360@gmail.com>
2021-09-30 16:38:37 -0300

    Update: Test modules

--- pysid/identification/test_pemethod.py
+++ pysid/identification/test_pemethod.py
@@ -1,11 +1,13 @@
-# Testing modules for pemethod.py using pytest
 # @author: lima84
-# Docstring?
+"""
+    Testing modules for pemethod.py using pytest
+"""
 
 import pytest
-from numpy import ndarray
+from numpy import array, ndarray
 from numpy.random import rand, randn
 from scipy.signal import lfilter
+from pysid.identification.pemethod import arx
 
 # ------------------- PYTEST -------------------
 #   Running test modules with Pytest:
@@ -24,11 +26,19 @@ from scipy.signal import lfilter
 # ------------------- FIXTURES -------------------
 
 # Defines a set of input and output data as a fixture
+
 @pytest.fixture
-def test_signals():
-    # True test system parameters
+def test_polynomials():
     Ao = [1, -1.2, 0.36]
     Bo = [0, 0.5, 0.1]
+
+    return [Ao, Bo]
+
+@pytest.fixture
+def test_signals(test_polynomials):
+    # True test system parameters
+    Ao = test_polynomials[0]
+    Bo = test_polynomials[1]
 
     # Replicates the following experiment:
     # y(t) = Go(q)*u(t) + Ho(q)*e(t),
@@ -38,27 +48,36 @@ def test_signals():
     e = 0.01*randn(N, 1)    # Emulates gaussian white noise with std = 0.01
 
     # Calculates the y ARX: G(q) = B(q)/A(q) and H(q) = 1/A(q)
-    y = lfilter(Bo, Ao, u, axis=0) + lfilter([1], Ao, e, axis=0)
+    y = lfilter(Bo, Ao, u, axis=0) #+ lfilter([1], Ao, e, axis=0)
 
-    return u, y
+    return [u, y, Ao, Bo]
 
 # Defines sets of arguments for tests that request the respective fixtures
-# Here, the values of na, nb and nk are varied
-@pytest.mark.parametrize("na, nb, nk", [(1, 0 , 1), ('1', 2, 3)])
+# Here, the values of na to nk are varied
+@pytest.mark.parametrize("na, nb, nc, nd, nf, nk", [(1, 0 , 1, 1, 0, 1), (1, 2, 3, 1, 0, 1)])
 
 # ------------------- TEST FUNCTIONS -------------------
 
-# Validates ARX model orders
+# Validates polynomial model orders
 # Every passed argument is a requested @pytest.fixture
-def test_arx_orders(na, nb, nk):
+def test_polynomial_orders(na, nb, nc, nd, nf, nk):
     # Checks the consistency of na 
-    assert isinstance(na, int)   
+    assert isinstance(na, int)
     
     # Checks the consistency of nb
-    assert isinstance(nb, int)   
+    assert isinstance(nb, int)
     
+    # Checks the consistency of nc
+    assert isinstance(nc, int)
+
     # Checks the consistency of nk
-    assert isinstance(nk, int) 
+    assert isinstance(nd, int)
+
+    # Checks the consistency of nk
+    assert isinstance(nf, int)
+
+    # Checks the consistency of nk
+    assert isinstance(nk, int)
 
 def test_data_type(test_signals):
     u = test_signals[0]
@@ -69,3 +88,18 @@ def test_data_type(test_signals):
 
     # Checks the consistency of y(t)
     assert isinstance(y, ndarray) or isinstance(y, list)
+
+#@pytest.mark.xfail
+def test_arx(test_signals):
+
+    u = test_signals[0]
+    y = test_signals[1]
+    Ao = array(test_signals[2])
+    Bo = array(test_signals[3])
+
+    A, B = arx(2,1,1,u,y)
+    
+    print(A.tolist() == Ao)
+    assert isinstance(A,ndarray)
+    assert isinstance(A.tolist(),list)
+    assert not isinstance(A,list)
