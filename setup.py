c0f0dca4c650f2ba9c7a8432c08f106301828720
Eduardo Mapurunga <edumapurunga@gmail.com>
2021-11-01 13:25:12 -0300

    Update setup.py - requires specific versions of numpy and scipy

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    install_requires=['numpy',
-                      'scipy'],
+    install_requires=['numpy>=1.19',
+                      'scipy>=1.4'],
     python_requires='>=3.6',
 )
