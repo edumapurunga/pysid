20e2ff21de349a0008b10ba27170787598128f7b
Eduardo Mapurunga <edumapurunga@gmail.com>
2021-11-01 13:19:40 -0300

    Update tseries.py - add spaces for readibility

--- pysid/identification/tseries.py
+++ pysid/identification/tseries.py
@@ -5,7 +5,7 @@ Module for estimate time series models (AR, MA, ARMA)
 author: @edumapurunga
 """
 
-#%%Imports
+# Imports
 from numpy import arange, array, append, asscalar, copy, count_nonzero, ones,\
 delete, dot, empty, sum, size, amax, matrix, concatenate, shape, zeros, kron,\
 eye, reshape, convolve, sqrt, where, nonzero, correlate, equal, ndarray, pi, \
@@ -15,12 +15,12 @@ from numpy.linalg import matrix_rank
 from scipy.signal import lfilter, periodogram
 from scipy.optimize import leastsq, least_squares
 import numpy.fft as fft
-#Internal imports
 from .pemethod import chckin
 from .pemethod import ls
-#%% functions
+# Internal imports
+# functions
 __all__ = ['ar', 'arma', 'ma']
-#%% implementations
+# implementations
 def ar(na, y, md = 'yw'):
     """
     This function estimate a AR model based on output data provided in the
@@ -33,24 +33,24 @@ def ar(na, y, md = 'yw'):
     na, _, _, _, _, _, _, y = chckin(na, [], [], [], [], [], y, y)
     Ny, ny = shape(y)
     na = na.item()
-    #Yule-Walker solution
+    # Yule-Walker solution
     if md == 'yw':
-        #Find the correlation
+        # Find the correlation
         R = correlate(y.reshape(Ny,), y.reshape(Ny,), "full")
-        #Biased Correlaton
+        # Biased Correlaton
         R = R/Ny
         R = R[0:na+1]
         A = levinson(R, na)
         A = A[-1]
-    #Burg Algorithm
+    # Burg Algorithm
     elif md == 'burg':
         A = burg(y, na)
-    #Prediction Error algorithm
+    # Prediction Error algorithm
     elif md == 'pem':
-        #Define the prediction error
+        # Define the prediction error
         def pe(theta, na, y):
             return lfilter(append([1], theta[0:na]), [1], y, axis=0)
-        #Least Squares Initialization
+        # Least Squares Initialization
         thetai = ls(na, -1, 0, y, y)[0]
         sol = least_squares(pe, thetai, gtol=1e-15, args=(na, y.reshape((Ny))))
         theta = sol.x
@@ -67,22 +67,22 @@ def arma(na, nb, y, md='pem'):
         na = array(na)
     if nb != 0:
         nb = array(nb)
-    #size
+    # size
     Ny, ny = shape(y)
-    #Hannan-Rissanen Algorithm
+    # Hannan-Rissanen Algorithm
     if md == 'hannan':
-        #Step 1: Estimate a high order AR
+        # Step 1: Estimate a high order AR
         n = 50
         Ar = ar(n, y, md='burg')
         ehat = lfilter(Ar, [1], y, axis=0)
-        #Step 2: Estimate an initial ARMA model
+        # Step 2: Estimate an initial ARMA model
         A1, B1 = ls(na, nb, 1, ehat, y)
-        #Step 3: Reestimate based on an approximation of ML
+        # Step 3: Reestimate based on an approximation of ML
         etil = lfilter(append([1], A1), [1], y, axis=0) - lfilter(append([0], B1), [1], ehat, axis=0)
-        #Predictors Inputs
+        # Predictors Inputs
         napa = lfilter([1], append([1], B1), y, axis=0)
         qsi = lfilter([1], append([1], B1), etil, axis=0)
-        #Predicted Output
+        # Predicted Output
         Y = etil + napa - qsi
         u = concatenate((-napa, qsi), axis=1)
         _, B = arx(0, [[na-1, nb]], [1, 1], u, Y)
@@ -90,12 +90,12 @@ def arma(na, nb, y, md='pem'):
         B = B[0, 1][1:]
         A = append([1], A)
         B = append([1], B)
-    #PEM algorithm
+    # PEM algorithm
     if md == 'pem':
-        #Define the prediction error
+        # Define the prediction error
         def pe(theta, na, nb, y):
             return lfilter(append([1], theta[0:na]), append([1], theta[na:]), y, axis=0)
-        #Least Squares Initialization
+        # Least Squares Initialization
         n = 50
         Ar = ar(n, y, md='burg')
         ehat = lfilter(Ar, [1], y, axis=0)
@@ -115,34 +115,34 @@ def ma(nb, y, md='durbin'):
     nb = array(nb)
     y = array(y)
     Ny, ny = shape(y)
-    #Durbin Method
+    # Durbin Method
     if md == 'durbin':
-        #Estimate a high order AR
+        # Estimate a high order AR
         n = 2*nb.item()
         Ar = ar(n, y, 'burg')
-        #Estimate the innovations
+        # Estimate the innovations
         ehat = lfilter(Ar, [1], y, axis=0)
         v = y - ehat
         B = ls(0, nb, 1, ehat, v)[1]
         B = append([1], B)
-    #Vocariance recursion method
+    # Vocariance recursion method
     if md == 'vrm':
         Psi = zeros((Ny,))
         Psi2= zeros((Ny,))
         t = arange(0, Ny)
         wk = 2*t*pi/Ny
-        #Estimate of the periodogram
+        # Estimate of the periodogram
         for p in range(0, Ny):
             Psi[p] = 1/Ny*absolute(sum(y*exp(-1j*wk[p]*t)))**2
         Psi2 = periodogram(y, scaling='spectrum', axis=0)[1]
-        #Estimate the vocariances
+        # Estimate the vocariances
         c = zeros((int(Ny/2),))
         c2 = absolute(fft.ifft(log(Psi2), axis=0))**2
         c2 = c2.reshape((101,))
         for k in range(0, int(Ny/2)):
             #TODO: Verify how to compute the ceptrum
             c[k] = 1/Ny*sum(real(log(Psi)*exp(-1j*wk[k]*t)))
-        #Estimate the MA parameters
+        # Estimate the MA parameters
         b = zeros((nb+2,))
         b[0] = 1
         for j in range(1, nb+2):
