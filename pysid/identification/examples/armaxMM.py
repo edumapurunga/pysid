1b38130c5f84abb012c986961a501a1d66c69180
Eduardo Mapurunga <edumapurunga@gmail.com>
2021-12-01 20:20:08 -0300

    Update examples/armaxMM.py - correct the data generating mechanism

--- pysid/identification/examples/armaxMM.py
+++ pysid/identification/examples/armaxMM.py
@@ -2,7 +2,7 @@
     In this example we use the pysid library to estimate a MIMO armax model
 """
 #Import Libraries
-from numpy import concatenate, dot, zeros
+from numpy import array, convolve, concatenate, zeros
 from numpy.random import rand, randn #To generate the experiment
 from scipy.signal import lfilter     #To generate the data
 from pysid import armax                #To estimate an arx model
@@ -15,40 +15,49 @@ ny = 2
 na = [[2, 2], [2, 2]]       #This variable must be (ny x ny)
 nb = [[1, 1], [1, 1]]       #This variable must be (ny x nu)
 nk = [[1, 1], [1, 1]]       #This variable must be (ny x nu)
-nc = [2, 2]                 #This variable must be (ny x 1)
+nc = [[2], [2]]                 #This variable must be (ny x 1)
 #with the following true parameters
-A1o  = [1, -1.2, 0.36]
-A12o = [0, 0.09, -0.1]
-A2o  = [1, -1.6, 0.64]
-A21o = [0, 0.2, -0.01]
-B11o = [0, 0.5, 0.4]
-B12o = [0, 0.9, 0.8]
-B21o = [0, 0.2,-0.3]
-B22o = [0, 0.1,-0.8]
-C1o  = [1, 0.8,-0.1]
-C2o  = [1, 0.9,-0.2]
+A1o  = array([1, -1.2, 0.36])
+A12o = array([0, 0.09, -0.1])
+A2o  = array([1, -1.6, 0.64])
+A21o = array([0, 0.2, -0.01])
+B11o = array([0, 0.5, 0.4])
+B12o = array([0, 0.9, 0.8])
+B21o = array([0, 0.2,-0.3])
+B22o = array([0, 0.1,-0.8])
+C1o  = array([1, 0.8,-0.1])
+C2o  = array([1, 0.9,-0.2])
 #True parameter vector
 thetao = [-1.2, 0.36, 0.5, 0.4, 0.2, -0.3, 0.8, -0.1]
-#Generate the experiment
-#The true system is generates by the following relation:
-# S: y(t) = Go(q)*u(t) + Ho(q)*e(t),
-#with u(t) the input and e white noise.
-#Number of Samples
-N = 200
-#Take u as uniform
+# Generate the experiment
+# The true system is generates by the following relation:
+#    S: y(t) = Go(q)*u(t) + Ho(q)*e(t),
+#       with u(t) the input and e white noise.
+# Number of Samples
+N = 1000
+# Take u as uniform
 u = -1 + 2*rand(N, nu)
-#Generate gaussian white noise with standat deviation 0.01
+# Generate gaussian white noise with standat deviation 0.01
 e = 0.01*randn(N, ny)
-#Calculate the y through S (ARX: G(q) = B(q)/A(q) and H(q) = 1/A(q))
-#Calculate the y through S (ARX: G(q) = B(q)/A(q) and H(q) = 1/A(q))
-y1 = zeros((N, 1))
-y2 = zeros((N, 1))
-v1 = lfilter(C1o, [1], e[:,0:1], axis=0)
-v2 = lfilter(C2o, [1], e[:,1:2], axis=0)
-#Simulate the true process
-for i in range(2, N):
-    y1[i] = -dot(A1o[1:3] , y1[i-2:i][::-1]) - dot(A12o[1:3],y2[i-2:i][::-1]) + dot(B11o[1:3], u[i-2:i, 0][::-1]) + dot(B12o[1:3], u[i-2:i, 1][::-1])
-    y2[i] = -dot(A21o[1:3], y1[i-2:i][::-1]) - dot(A2o[1:3], y2[i-2:i][::-1]) + dot(B21o[1:3], u[i-2:i, 0][::-1]) + dot(B22o[1:3], u[i-2:i, 1][::-1])
-y = concatenate((y1+v1, y2+v2), axis=1)
+# ARMAX A**-1*B u + A**-1*C
+# Calculate the y through S (ARX: G(q) = B(q)/A(q) and H(q) = 1/A(q))
+# Calculate the y through S (ARX: G(q) = B(q)/A(q) and H(q) = 1/A(q))
+
+det = convolve(A1o, A2o) - convolve(A12o, A21o) 
+y1 = lfilter(convolve(A2o, B11o), det, u[:, 0:1], axis=0) + \
+     lfilter(convolve(-A12o, B21o), det, u[:, 0:1], axis=0) + \
+     lfilter(convolve(A2o, B12o), det, u[:, 1:2], axis=0) + \
+     lfilter(convolve(-A12o, B22o), det, u[:, 1:2], axis=0) + \
+     lfilter(convolve(A2o, C1o), det, e[:, 0:1], axis=0) + \
+     lfilter(convolve(-A12o, C2o), det, e[:, 1:2], axis=0)
+y2 = lfilter(convolve(-A21o, B11o), det, u[:, 0:1], axis=0) + \
+     lfilter(convolve(A1o, B21o), det, u[:, 0:1], axis=0) + \
+     lfilter(convolve(-A21o, B12o), det, u[:, 1:2], axis=0) + \
+     lfilter(convolve(A1o, B22o), det, u[:, 1:2], axis=0) + \
+     lfilter(convolve(-A21o, C1o), det, e[:, 0:1], axis=0) + \
+     lfilter(convolve(A1o, C2o), det, e[:, 1:2], axis=0)
+
+y = concatenate((y1, y2), axis=1)
+
 #Estimate the model and get only the parameters
 A, B, C = armax(na, nb, nc, nk, u, y)
